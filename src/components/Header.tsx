17ef1ed3a68cc2f768d5862bafe746ff12734fd0
lucjastozek <lucjastozek@gmail.com>
2025-08-20 22:27:19 +0200

    add a11y workflow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,5 +3,9 @@ interface HeaderProps {
 }
 
 export default function Header({ text }: HeaderProps): JSX.Element {
-  return <h1>{text}</h1>;
+  return (
+    <header>
+      <h1>{text}</h1>
+    </header>
+  );
 }
