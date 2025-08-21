d0bb423fb6c37d0a4462e1cca3f1f7ad71c24edd
Łucja Stożek <118857133+lucjastozek@users.noreply.github.com>
2025-08-22 00:59:29 +0200

    add basic e2e tests (#4)
    * small fixes
    
    * add basic e2e tests
    
    * fix playwright tests in github ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,5 +15,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/tests/setup.ts",
+    exclude: ["**/node_modules/**", "**/e2e/**", "**/dist/**"],
   },
 });
